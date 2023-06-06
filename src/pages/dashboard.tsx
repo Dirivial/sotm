807d53d769500d9ef2fe0e4a4a54eb65f6674c6e
Dirivial <alexander.kadeby@gmail.com>
2023-06-06 13:59:17 +0200

    Removed unnecessary code

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,10 @@
 import { type NextPage } from "next";
-import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
-import { api } from "~/utils/api";
+//import { api } from "~/utils/api";
 
 const Dashboard: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  //const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
