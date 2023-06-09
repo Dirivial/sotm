ac056bf8247719ecfdb3c243827c234ca477ee0a
Dirivial <alexander.kadeby@gmail.com>
2023-06-10 01:03:19 +0200

    Added remove button

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,13 +1,15 @@
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import Image from "next/image";
 
 interface Props {
   dragId: string;
   content: string;
-  className: string;
+  removeItem?: (id: string) => void;
+  opacity: string;
 }
 
-const Item: React.FC<Props> = ({ dragId, content, className }) => {
+const Item: React.FC<Props> = ({ dragId, content, removeItem, opacity }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: dragId });
 
@@ -16,26 +18,44 @@ const Item: React.FC<Props> = ({ dragId, content, className }) => {
     transition,
   };
 
+  const handleRemove = () => {
+    if (removeItem) {
+      removeItem(dragId);
+    }
+  };
+
   return (
     <div
       ref={setNodeRef}
       style={style}
-      className={
-        "my-3 flex flex-row justify-center rounded-sm bg-gradient-to-br from-[#035388] to-[#2BB0ED] align-middle  shadow-md" +
-        " " +
-        className
-      }
+      className={`my-3 flex flex-row justify-center rounded-sm bg-gradient-to-br from-[#035388] to-[#2BB0ED] align-middle  shadow-md ${opacity}`}
     >
       <div className="flex flex-col justify-center border-r-2 px-1">
         <p className="text-white">30 min</p>
       </div>
       <div
-        className={" h-[60px] flex-grow p-4 text-white "}
+        className={"h-[60px] flex-grow p-4 text-white"}
         data-id={dragId}
         {...attributes}
         {...listeners}
       >
         {content}
+      </div>
+      <div className="grid grid-cols-1 grid-rows-2">
+        <button className="w-5" onClick={handleRemove}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="icon-close fill-white transition-colors ease-in-out hover:fill-supportingRed6"
+          >
+            <path
+              className="secondary"
+              fill-rule="evenodd"
+              d="M15.78 14.36a1 1 0 0 1-1.42 1.42l-2.82-2.83-2.83 2.83a1 1 0 1 1-1.42-1.42l2.83-2.82L7.3 8.7a1 1 0 0 1 1.42-1.42l2.83 2.83 2.82-2.83a1 1 0 0 1 1.42 1.42l-2.83 2.83 2.83 2.82z"
+            />
+          </svg>
+        </button>
+        <div className="rounded-sm" {...attributes} {...listeners} />
       </div>
     </div>
   );
