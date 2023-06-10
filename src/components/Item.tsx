62ef26315e7853d675e2626f7d04921d98e7eb21
Dirivial <alexander.kadeby@gmail.com>
2023-06-10 13:24:33 +0200

    Extracted component from Item

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,7 +1,7 @@
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import { Disclosure, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
+import { useState } from "react";
+import { SetTimeDisclosure } from "./SetTimeDisclosure";
 
 interface Props {
   dragId: string;
@@ -34,8 +34,7 @@ const Item: React.FC<Props> = ({ dragId, content, removeItem, opacity }) => {
       className={`my-3 flex flex-row justify-center rounded-md bg-gradient-to-br from-[#035388] to-[#2BB0ED] align-middle  shadow-md ${opacity}`}
     >
       <div className="flex w-1/5 flex-none flex-row border-r-2">
-        {/* <SetTimePopover initialTime={time} updateTime={setTime} /> */}
-        <Example initialTime={time} updateTime={setTime} />
+        <SetTimeDisclosure initialTime={time} updateTime={setTime} />
       </div>
       <div
         className={"h-[60px] flex-grow p-4 text-white"}
@@ -66,77 +65,3 @@ const Item: React.FC<Props> = ({ dragId, content, removeItem, opacity }) => {
 };
 
 export default Item;
-
-interface SetTimeProps {
-  initialTime: string;
-  updateTime: (time: string) => void;
-}
-
-function Example({ initialTime, updateTime }: SetTimeProps) {
-  const [timeH, setTimeH] = useState(Number(initialTime.slice(0, 2)));
-  const [timeM, setTimeM] = useState(Number(initialTime.slice(3)));
-
-  const handleTimeChange = (hours: boolean, value: string) => {
-    if (hours) {
-      setTimeH(Number(value));
-      updateTime(
-        `${Number(value) < 10 ? "0" + value : value}:${
-          timeM < 10 ? "0" + timeM.toString() : timeM
-        }`
-      );
-    } else {
-      setTimeM(Number(value));
-      updateTime(
-        `${timeH < 10 ? "0" + timeH.toString() : timeH}:${
-          Number(value) < 10 ? "0" + value : value
-        }`
-      );
-    }
-  };
-
-  return (
-    <div className="m-auto w-4/5">
-      <div className="rounded-2xl">
-        <Disclosure>
-          {() => (
-            <>
-              <Disclosure.Button className="flex min-w-full justify-between rounded-lg py-2 text-left text-sm font-medium text-white opacity-90 hover:bg-supportingBlue3 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <p className="m-auto">{initialTime}</p>
-              </Disclosure.Button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-200"
-                enterFrom="opacity-0 translate-y-1"
-                enterTo="opacity-100 translate-y-0"
-                leave="transition ease-in duration-150"
-                leaveFrom="opacity-100 translate-y-0"
-                leaveTo="opacity-0 translate-y-1"
-              >
-                <Disclosure.Panel className="absolute z-20 mt-5 grid grid-cols-2 grid-rows-2 gap-2 rounded-lg bg-neutral9 px-4 py-4 text-sm text-neutral1">
-                  <label>Hours</label>
-                  <input
-                    className="rounded-md px-1 outline-none"
-                    type="number"
-                    min={0}
-                    max={24}
-                    value={timeH}
-                    onChange={(e) => handleTimeChange(true, e.target.value)}
-                  />
-                  <label className="inline-block">Minutes</label>
-                  <input
-                    className="rounded-md px-1 outline-none"
-                    type="number"
-                    min={0}
-                    max={59}
-                    value={timeM}
-                    onChange={(e) => handleTimeChange(false, e.target.value)}
-                  />
-                </Disclosure.Panel>
-              </Transition>
-            </>
-          )}
-        </Disclosure>
-      </div>
-    </div>
-  );
-}
---
+++ src/components/SetTimeDisclosure.tsx
@@ -0,0 +1,76 @@
+import { Disclosure, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+
+export interface Props {
+  initialTime: string;
+  updateTime: (time: string) => void;
+}
+
+export function SetTimeDisclosure({ initialTime, updateTime }: Props) {
+  const [timeH, setTimeH] = useState(Number(initialTime.slice(0, 2)));
+  const [timeM, setTimeM] = useState(Number(initialTime.slice(3)));
+
+  const handleTimeChange = (hours: boolean, value: string) => {
+    if (hours) {
+      setTimeH(Number(value));
+      updateTime(
+        `${Number(value) < 10 ? "0" + value : value}:${
+          timeM < 10 ? "0" + timeM.toString() : timeM
+        }`
+      );
+    } else {
+      setTimeM(Number(value));
+      updateTime(
+        `${timeH < 10 ? "0" + timeH.toString() : timeH}:${
+          Number(value) < 10 ? "0" + value : value
+        }`
+      );
+    }
+  };
+
+  return (
+    <div className="m-auto w-4/5">
+      <div className="rounded-2xl">
+        <Disclosure>
+          {() => (
+            <>
+              <Disclosure.Button className="flex min-w-full justify-between rounded-lg py-2 text-left text-sm font-medium text-white opacity-90 hover:bg-supportingBlue3 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <p className="m-auto">{initialTime}</p>
+              </Disclosure.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <Disclosure.Panel className="absolute z-20 mt-5 grid grid-cols-2 grid-rows-2 gap-2 rounded-lg bg-neutral9 px-4 py-4 text-sm text-neutral1">
+                  <label>Hours</label>
+                  <input
+                    className="rounded-md px-1 outline-none"
+                    type="number"
+                    min={0}
+                    max={24}
+                    value={timeH}
+                    onChange={(e) => handleTimeChange(true, e.target.value)}
+                  />
+                  <label className="inline-block">Minutes</label>
+                  <input
+                    className="rounded-md px-1 outline-none"
+                    type="number"
+                    min={0}
+                    max={59}
+                    value={timeM}
+                    onChange={(e) => handleTimeChange(false, e.target.value)}
+                  />
+                </Disclosure.Panel>
+              </Transition>
+            </>
+          )}
+        </Disclosure>
+      </div>
+    </div>
+  );
+}
