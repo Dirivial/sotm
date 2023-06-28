580c6cbcc5ded5ba7d003bfcb2ab9b8d12be4462
Dirivial <alexander.kadeby@gmail.com>
2023-06-28 21:23:19 +0200

    Added export

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-const userRouter = createTRPCRouter({
+export const userRouter = createTRPCRouter({
   getById: publicProcedure.input(z.string()).query(({ ctx, input }) => {
     return ctx.prisma.user.findFirst({
       where: {
